1a843a14ca13260be415d9df7c4ffca48c2491a1
aycollapse <81759055+aycollapse@users.noreply.github.com>
2025-05-27 21:58:29 +0200

    aggiunto script per pulire db velocemente (solo per sviluppo)

---
+++ utils/drop_tables.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS 
+    media,
+    saga,
+    serie_tv,
+    film,
+    episodio,
+    membro,
+    utente,
+    licenza,
+    casting_media_membro,
+    media_visti_utente
+CASCADE;
